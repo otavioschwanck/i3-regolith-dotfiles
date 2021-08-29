8c80ce1fa9323f1631aeb9db2227179c3b38973c
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2021-08-29 02:24:04 -0300

    improve hud script

--- home/.config/regolith/i3/start-hud.sh
+++ home/.config/regolith/i3/start-hud.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sleep 1
+sleep 2
 i3-msg "workspace number 1"
+sleep 0.01
 i3-msg "workspace number 2"
+sleep 0.01
 i3-msg "workspace number 3"
+sleep 0.01
 i3-msg "workspace number 4"
+sleep 0.01
 i3-msg "workspace number 5"
+sleep 0.01
 i3-msg "workspace number 6"
+sleep 0.01
 i3-msg "workspace number 7"
+sleep 0.01
 i3-msg "workspace number 8"
+sleep 0.01
 i3-msg "workspace number 9"
-i3-msg "workspace number 1"
-sleep 1
+sleep 3
 /usr/lib/mate-hud/mate-hud > /dev/null 2>&1 &
